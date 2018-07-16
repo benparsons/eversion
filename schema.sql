23e9385a44d6aac15d67eaece589bea7eb13c99f
Ben Parsons <ben@bpulse.co.uk>
2018-07-17 00:19:51 +0100

    sanitise schema.sql

--- schema.sql
+++ schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE events(type TEXT, order_id TEXT, order_type TEXT, size REAL, price REAL, side TEXT, product_id TEXT, sequence INTEGER, user_id TEXT, profile_id TEXT, time TEXT, remaining_size REAL, reason TEXT);
 CREATE TABLE accounts(eth_available REAL, eth_hold REAL, btc_available REAL, btc_hold REAL, time TEXT);
-CREATE TABLE IF NOT EXISTS "orders" (
+CREATE TABLE orders(
 	`type`	TEXT,
 	`order_id`	TEXT,
 	`order_type`	TEXT,
@@ -8,13 +8,12 @@ CREATE TABLE IF NOT EXISTS "orders" (
 	`price`	REAL,
 	`side`	TEXT,
 	`product_id`	TEXT,
-	`sequence`	INTEGER,
-	`user_id`	TEXT,
-	`profile_id`	TEXT,
-	`time`	TEXT,
 	`remaining_size`	REAL,
 	`reason`	TEXT,
-	`last_updated`	TEXT
+	`expire_time` TEXT,
+	`post_only` INTEGER,
+    `settled` INTEGER,
+	`last_updated` TEXT
 );
 CREATE UNIQUE INDEX `orders_unique_order_id` ON `orders` (
 	`order_id`
